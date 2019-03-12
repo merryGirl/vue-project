d00ad78c6c830555c42298a187802c406384aac0
merryGirl <2283906227@qq.com>
2019-03-12 19:05:16 +0800

    change the style of the index page

--- src/components/index.vue
+++ src/components/index.vue
@@ -2,43 +2,41 @@
     <div class="index-content">
         <div class="header"> 
             <div class="header-title">优质漫画推荐</div>
-            <div class="user-content">
+            <div class="user-info">
                 <img src="../assets/img/head_picture.png" alt="">
             </div>
         </div>
         <div class="content">
-            <el-row>
-                <!-- left nav -->
-                <el-col :span="5">
-                    <el-menu default-active="2" @select="handleSelect">
-                        <el-submenu index="1">
-                            <template slot="title">
-                                <i class="el-icon-view"></i>
-                                番剧
-                            </template>
-                            <el-menu-item index="1-1">国漫</el-menu-item>
-                            <el-menu-item index="1-2">日漫</el-menu-item>
-                        </el-submenu>
-                        <el-menu-item index="2">
-                            <i class="el-icon-star-off"></i>
-                            豆瓣评分
-                        </el-menu-item>
-                        <el-menu-item index="3">
-                            <i class="el-icon-info"></i>
-                            个人信息
-                        </el-menu-item>
-                    </el-menu>
-                </el-col>
+            <!-- left nav -->
+            <div class="left-menu">
+                <el-menu default-active="2" @select="handleSelect">
+                    <el-submenu index="1">
+                        <template slot="title">
+                            <i class="el-icon-view"></i>
+                            番剧
+                        </template>
+                        <el-menu-item index="1-1">国漫</el-menu-item>
+                        <el-menu-item index="1-2">日漫</el-menu-item>
+                    </el-submenu>
+                    <el-menu-item index="2">
+                        <i class="el-icon-star-off"></i>
+                        豆瓣评分
+                    </el-menu-item>
+                    <el-menu-item index="3">
+                        <i class="el-icon-info"></i>
+                        个人信息
+                    </el-menu-item>
+                </el-menu>
+            </div>
 
-                <!-- right content -->
-                <el-col :span="19">
-                    <component 
-                        :is="templateShow" 
-                        :menuIndex="menuIndex"
-                        :cartoomData="cartoomData">
-                    </component>
-                </el-col>
-            </el-row>
+            <!-- right content -->
+            <div class="right-content">
+                <component 
+                    :is="templateShow" 
+                    :menuIndex="menuIndex"
+                    :cartoomData="cartoomData">
+                </component>
+            </div>
         </div>
     </div>
 </template>
@@ -103,6 +101,19 @@ export default {
 };
 </script>
 
+<style lang="less">
+.index-content {
+    .content {
+        ul.el-menu {
+            height: 100%;
+            .el-submenu .el-menu-item {
+                min-width: 180px;
+            }
+        }
+    }
+}
+</style>
+
 <style lang="less" scoped>
 .index-content {
     .header {
@@ -114,17 +125,34 @@ export default {
         display: flex;
         justify-content: space-between;
         align-items: center;
+        background-color: @white;
         border-bottom: 1px solid gray;
 
         .header-title {
             margin-left: 30px;
         }
-        .user-content {
+        .user-info {
             img {
                 width: 50px;
                 margin-right: 30px;
             }
         }
     }
+
+    .content {
+        .left-menu {
+            width: 180px;
+            height: 100%;
+            position: fixed;
+            left: 0;
+            top: 61px;
+        }
+
+        .right-content {
+            width: calc(100% - 180px);
+            padding-left: 180px;
+        }
+    }
 }
 </style>
+
